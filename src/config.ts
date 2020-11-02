3985a20226f2b55fe77ea7e6c8022fc83787b6cb
unknown <travbaha@gmail.com>
2020-11-02 14:15:38 -0500

    vscode

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+//configuration information 
+export const Config={
+    serverport: process.env.PORT || 3000,
+    secret: process.env.SECRET || "some-secret-goes-here",
+    tokenLife: 1800,
+    url: process.env.MONGOURL || "mongodb://localhost:27017/"    
+};
